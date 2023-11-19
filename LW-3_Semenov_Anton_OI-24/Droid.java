d6402d59e50b5f5092dccf402fce20b3677c1e76
Mirai4ik <87390053+Mirai4ik@users.noreply.github.com>
2023-11-20 01:10:49 +0200

    Create Droid.java

---
+++ LW-3_Semenov_Anton_OI-24/Droid.java
@@ -0,0 +1,40 @@
+public class Droid {
+    protected String name;
+    protected int health;
+    protected int damage;
+
+    public Droid(String name, int health, int damage) {
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Droid{" +
+                "name='" + name + '\'' +
+                ", health=" + health +
+                ", damage=" + damage +
+                '}';
+    }
+}
