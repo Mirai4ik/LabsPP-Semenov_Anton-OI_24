04be819288a9eebd24e7404ddcd391ccc7a74fc4
Mirai4ik <87390053+Mirai4ik@users.noreply.github.com>
2023-11-20 01:10:03 +0200

    Update BattleDroidsGame.java

--- LW-3_Semenov_Anton_OI-24/BattleDroidsGame.java
+++ LW-3_Semenov_Anton_OI-24/BattleDroidsGame.java
@@ -5,59 +5,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
-class Droid {
-    protected String name;
-    protected int health;
-    protected int damage;
-
-    public Droid(String name, int health, int damage) {
-        this.name = name;
-        this.health = health;
-        this.damage = damage;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public int getDamage() {
-        return damage;
-    }
-
-    public void takeDamage(int damage) {
-        health -= damage;
-    }
-
-    public boolean isAlive() {
-        return health > 0;
-    }
-
-    @Override
-    public String toString() {
-        return "Droid{" +
-                "name='" + name + '\'' +
-                ", health=" + health +
-                ", damage=" + damage +
-                '}';
-    }
-}
-
-class Tank extends Droid {
-    public Tank(String name) {
-        super(name, 150, 20);
-    }
-}
-
-class Assault extends Droid {
-    public Assault(String name) {
-        super(name, 100, 30);
-    }
-}
-
 public class BattleDroidsGame {
     private static Random random = new Random();
     private static List<Droid> droids = new ArrayList<>();
